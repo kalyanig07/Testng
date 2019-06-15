7142c7840e3f1118c8ec86db2d04799a72758d2d
group001 <group001@infoway.com>
2019-06-15 08:34:46 +0530

    junit to testNG project

---
+++ kalyani123/src/main/java/com/example/kalyani123/Kalyani123Application.java
@@ -0,0 +1,13 @@
+package com.example.kalyani123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kalyani123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kalyani123Application.class, args);
+	}
+
+}
---
+++ kalyani123/src/test/java/com/example/kalyani123/Kalyani123ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.kalyani123;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class Kalyani123ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
